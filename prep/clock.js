0bea0cc2660da9aeed23d0381b27c4346d7c2589
Konvaly <konvaly@gmail.com>
2025-11-04 01:33:03 +0000

    making an assertion

--- prep/clock.js
+++ prep/clock.js
@@ -1,1 +1,17 @@
-function formatAs12HourClock() {}
+function formatAs12HourClock(time) {
+  return `${time} am`;
+}
+
+const currentOutput = formatAs12HourClock("08:00");
+const targetOutput = "08:00 am";
+console.assert(
+  currentOutput === targetOutput,
+  `current output: ${currentOutput}, target output: ${targetOutput}`
+);
+
+const currentOutput2 = formatAs12HourClock("23:00");
+const targetOutput2 = "11:00 pm";
+console.assert(
+  currentOutput2 === targetOutput2,
+  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
